6acea318b3100cdecb42ed60ff30e42453199ef6
daisuke.okada <okada.daisuke@lmi.ne.jp>
2020-06-12 02:58:17 +0000

    CRUD user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	before_action :current_user
 	
 	include SessionsHelper
 	
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :require_user_logged_in, only: [:index, :show]
+	before_action :require_user_logged_in, only: [:index, :show, :edit]
+	before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 	
 	def index
 		@users_pos = User.where(mind: "positive").order(id: :desc)
@@ -27,16 +28,37 @@ class UsersController < ApplicationController
 		
 	end
 	def edit
-		
+		@user = User.find(params[:id])
 	end
 	def update
+		@user = User.find(params[:id])
 		
+		if @user.update(user_params)
+			flash[:success] = "User updated successfully"
+			redirect_to @user
+		else
+			flash[:danger] = "User update failed"
+			render :edit
+		end
 	end
 	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:danger] = "Successfully deleted"
+		redirect_to root_path
 		
 	end
 	
+	private
 	
+	def ensure_correct_user
+		if @current_user.id != params[:id].to_i
+			flash[:danger] = "you required authorizating"
+			redirect_to users_path
+			
+		end 
+		
+	end 
 	
 	#Strong_parameter
 	def user_params
